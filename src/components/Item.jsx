90fcc5cc3a51cb01fe2074894685015647d15f0b
Nzy <nidal.chowdhury1@gmail.com>
2023-04-11 15:40:41 +1000

    init

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import "./Item.css";
 
 function Item({ name, description, price, currImg }) {
-
   function display() {
-    if ((name === "")) {
+    if (name === "") {
       return "none";
     } else {
-      return '1px solid lightgray'
+      return "1px solid lightgray";
     }
   }
 
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
-    <div className="item" style={{ border: display()}}>
-      <img className="item-img" src={currImg} alt="" />
+    <div className="item" style={{ border: display() }}>
+      {isLoading && (
+        <div
+          className="skeleton-box"
+          style={{ width: "40%", height: "100%" }}
+        ></div>
+      )}
+      <img
+        className="item-img"
+        src={currImg}
+        alt=""
+        onLoad={() => setIsLoading(false)}
+      />
       <div className="item-right">
         <h1>{name}</h1>
         {description && <p className="item-description">{description}</p>}
